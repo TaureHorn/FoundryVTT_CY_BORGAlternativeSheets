ff0bc6ead5ca08bedf6638ba35fbf525eda52954
TaureHorn <6367950+TaureHorn@users.noreply.github.com>
2025-01-17 11:43:59 +0000

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# CY_BORG Compact Sheets
+Adds alternatives sheets for the NPC, Vehicle, Item and Character for the CY_BORG system in FoundryVTT.
---
+++ cy-borg-compact-sheets.js
@@ -0,0 +1,74 @@
+console.log('cy-borg-compact-sheets >>> init')
+
+const cyborgSheets = {}
+
+function getCyborgSheets() {
+    const actorSheetNames = ['CYCharacterSheet', 'CYNpcSheet', 'CYVehicleSheet']
+    const itemSheetName = 'CYItemSheet'
+
+    actorSheetNames.forEach(name => {
+        cyborgSheets[name] = Actors.registeredSheets.find(sheet => sheet.name === name)
+    })
+    cyborgSheets[itemSheetName] = Items.registeredSheets.find(sheet => sheet.name === itemSheetName)
+}
+
+Hooks.once('ready', async () => {
+    getCyborgSheets()
+    console.log('Hooks.once ready', cyborgSheets)
+
+    class CompactCYNpcSheet extends cyborgSheets.CYNpcSheet {
+        static get defaultOptions() {
+            return foundry.utils.mergeObject(super.defaultOptions, {
+                template: "modules/cy-borg-compact-sheets/templates/npc-sheet.html"
+            })
+        }
+    }
+
+    class CompactCYCharacterSheet extends cyborgSheets.CYCharacterSheet {
+        static get defaultOptions() {
+            return foundry.utils.mergeObject(super.defaultOptions, {
+                template: "modules/cy-borg-compact-sheets/templates/character-sheet.html"
+            })
+        }
+    }
+
+    class CompactCYVehicleSheet extends cyborgSheets.CYVehicleSheet {
+        static get defaultOptions() {
+            return foundry.utils.mergeObject(super.defaultOptions, {
+                template: "modules/cy-borg-compact-sheets/templates/vehicle-sheet.html"
+            })
+        }
+    }
+
+    class CompactCYItemSheet extends cyborgSheets.CYItemSheet {
+        static get defaultOptions() {
+            return foundry.utils.mergeObject(super.defaultOptions, {
+                template: "modules/cy-borg-compact-sheets/templates/item-sheet.html"
+            })
+        }
+    }
+
+    Actors.registerSheet('cy-borg', CompactCYNpcSheet, {
+        types: ['npc'],
+        makeDefault: false,
+        label: 'Compact CY_BORG NPC Sheet'
+    })
+
+    Actors.registerSheet('cy-borg', CompactCYCharacterSheet, {
+        types: ['character'],
+        makeDefault: false,
+        label: 'Compact CY_BORG Character Sheet'
+    })
+
+    Actors.registerSheet('cy-borg', CompactCYVehicleSheet, {
+        types: ['vehicle'],
+        makeDefault: false,
+        label: 'Compact CY_BORG Vehicle Sheet'
+    })
+
+    Items.registerSheet('cy-borg', CompactCYItemSheet, {
+        makeDefault: false,
+        label: 'Compact CY_BORG NPC Sheet'
+    })
+})
+
